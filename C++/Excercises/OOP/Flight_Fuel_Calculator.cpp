bf1874c2dadf8e42a439aa49bebfd584eed9b19e
Parth Vighne <parthvighne@gmail.com>
2021-12-22 18:27:30 +0530

    Flight_Fuel_Calculator

---
+++ C++/Excercises/OOP/Flight_Fuel_Calculator.cpp
@@ -0,0 +1,55 @@
+// Defining a class Flight with the following specifications
+// Data Memebers : int flightNumber, string Destination,float Distance,float Fuel
+// Member Fuctions : CalFuel()
+//                  Distance                Fuel
+//                   <=1000                 500
+//                >1000 & <=2000            1100
+//                    >2000                 2200
+// Public Members :
+// Feed_Info()
+// Calc Fuel
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+class Flight
+{
+    int FlightNumber;
+    string Destination;
+    float Distance;
+    float Fuel;
+
+public:
+    void Feed_Info();
+    void Calc_Fuel();
+};
+
+void Flight ::Feed_Info()
+{
+    cout << "\nEnter Flight Number : ";
+    cin >> FlightNumber;
+    cout << "\nEnter Destination : ";
+    cin >> Destination;
+    cout << "\nEnter Distance : ";
+    cin >> Distance;
+}
+
+void Flight::Calc_Fuel()
+{
+    if (Distance <= 1000)
+        Fuel = 500;
+    else if (Distance > 1000 && Distance <= 2000)
+        Fuel = 1100;
+    else
+        Fuel = 2200;
+
+    cout << "\nAmount of fuel required : " << Fuel << " kilograms";
+}
+
+int main()
+{
+    Flight A;
+    A.Feed_Info();
+    A.Calc_Fuel();
+    return 0;
+}
