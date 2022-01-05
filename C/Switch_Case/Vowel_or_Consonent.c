289f415b883a0f2cf9e1ac316300380b6f507ebe
Parth Vighne <parthvighne@gmail.com>
2022-01-05 23:44:34 +0530

    Vowel_or_Consonent

---
+++ C/Switch_Case/Vowel_or_Consonent.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+
+int main()
+{
+    char ch;
+    printf("\nEnter letter : ");
+    scanf("%c", &ch);
+    switch (ch)
+    {
+    case 'a':
+    {
+        printf("\nIt is a Vowel.");
+        break;
+    }
+    case 'A':
+    {
+        printf("\nIt is a Vowel.");
+        break;
+    }
+    case 'e':
+    {
+        printf("\nIt is a Vowel.");
+        break;
+    }
+    case 'E':
+    {
+        printf("\nIt is a Vowel.");
+        break;
+    }
+    case 'i':
+    {
+        printf("\nIt is a Vowel.");
+        break;
+    }
+    case 'I':
+    {
+        printf("\nIt is a Vowel.");
+        break;
+    }
+    case 'o':
+    {
+        printf("\nIt is a Vowel.");
+        break;
+    }
+    case 'O':
+    {
+        printf("\nIt is a Vowel.");
+        break;
+    }
+    case 'u':
+    {
+        printf("\nIt is a Vowel.");
+        break;
+    }
+    case 'U':
+    {
+        printf("\nIt is a Vowel.");
+        break;
+    }
+    default:
+    {
+        printf("\nIt is not a vowel.");
+        break;
+    }
+    }
+
+    return 0;
+}
