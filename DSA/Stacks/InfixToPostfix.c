a37f428eaf2983f8df50419949dab003dc5ac95f
Parth Vighne <parthvighne@gmail.com>
2022-05-13 21:21:59 +0530

    Infix To postfix using stack

---
+++ DSA/Stacks/InfixToPostfix.c
@@ -0,0 +1,122 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct stack
+{
+    int size;
+    int top;
+    char *arr;
+};
+
+int isEmpty(struct stack *ptr)
+{
+    if (ptr->top == -1)
+        return 1;
+    else
+        return 0;
+}
+
+int isFull(struct stack *ptr)
+{
+    if (ptr->top == ptr->size - 1)
+        return 1;
+    else
+        return 0;
+}
+
+void push(struct stack *ptr, char value)
+{
+    if (isFull(ptr))
+        printf("\nStack overflow.");
+    else
+    {
+        ptr->top++;
+        ptr->arr[ptr->top] = value;
+    }
+}
+
+char pop(struct stack *ptr)
+{
+    if (isEmpty(ptr))
+        printf("\nStack Underflow.");
+    else
+    {
+        char val = ptr->arr[ptr->top];
+        ptr->top--;
+        return val;
+    }
+}
+
+char stackTop(struct stack *ptr)
+{
+    return ptr->arr[ptr->top];
+}
+
+int precedence(char ch)
+{
+    if (ch == '*' || ch == '/')
+        return 3;
+    else if (ch == '+' || ch == '-')
+        return 2;
+    else
+        return 0;
+}
+
+int isOperator(char ch)
+{
+    if (ch == '*' || ch == '/' || ch == '+' || ch == '-')
+        return 1;
+    else
+        return 0;
+}
+
+char *InfixToPostfix(char *infix)
+{
+    struct stack *s = (struct stack *)malloc(sizeof(struct stack));
+    s->size = 80;
+    s->top = -1;
+    s->arr = (char *)malloc(s->size * sizeof(char));
+
+    char *postfix = (char *)malloc((strlen(infix) + 1) * sizeof(char));
+    int i = 0; // Track infix traversal
+    int j = 0; // Track postfix addition
+
+    while (infix[i] != '\0')
+    {
+        if (!isOperator(infix[i]))
+        {
+            postfix[j] = infix[i];
+            i++;
+            j++;
+        }
+        else
+        {
+            if (precedence(infix[i]) > precedence(stackTop(s)))
+            {
+                push(s, infix[i]);
+                i++;
+            }
+            else
+            {
+                postfix[j] = pop(s);
+                j++;
+            }
+        }
+    }
+
+    while (!isEmpty(s))
+    {
+        postfix[j] = pop(s);
+        j++;
+    }
+    postfix[j] = '\0';
+    return postfix;
+}
+
+int main()
+{
+    char *infix = "x-y/z-k*d";
+    printf("\nPostfix is : %s", InfixToPostfix(infix));
+    return 0;
+}
