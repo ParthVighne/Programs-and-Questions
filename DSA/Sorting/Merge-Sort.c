35da55db3486ab29a49cfad609d78825ad65947b
Parth Vighne <parthvighne@gmail.com>
2022-06-21 20:37:21 +0530

    practice merge sort

--- DSA/Sorting/Merge-Sort.c
+++ DSA/Sorting/Merge-Sort.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+/*#include <stdio.h>
 
 void display(int A[], int n)
 {
@@ -69,6 +69,78 @@ int main()
     int n = 7;
     display(A, n);
     mergeSort(A, 0, 6);
+    display(A, n);
+    return 0;
+}*/
+
+#include <stdio.h>
+
+void display(int *A, int n)
+{
+    printf("\n- - - - - \n");
+    for (int i = 0; i < n; i++)
+        printf("%d ", A[i]);
+    printf("\n");
+}
+
+void merge(int A[], int low, int mid, int high)
+{
+    int i, j, k, B[100];
+    i = low;
+    j = mid + 1;
+    k = low;
+
+    while (i <= mid && j <= high) // compare elements from both sub-Arrays and copy the smaller one to array B
+    {
+        if (A[i] < A[j])
+        {
+            B[k] = A[i];
+            i++;
+            k++;
+        }
+        else
+        {
+            B[k] = A[j];
+            j++;
+            k++;
+        }
+    }
+
+    while (i <= mid) // copy rest elements to B
+    {
+        B[k] = A[i];
+        i++;
+        k++;
+    }
+
+    while (j <= high) // copy rest elements to B
+    {
+        B[k] = A[j];
+        j++;
+        k++;
+    }
+
+    for (int i = 0; i <= high; i++) // copy B to A
+        A[i] = B[i];
+}
+
+void mergeSort(int A[], int low, int high)
+{
+    if (low < high)
+    {
+        int mid = (low + high) / 2;
+        mergeSort(A, low, mid);
+        mergeSort(A, mid + 1, high);
+        merge(A, low, mid, high);
+    }
+}
+
+int main()
+{
+    int A[] = {56, 22, 1, 78, 34, 2, 6, 7, 13, 59};
+    int n = sizeof(A) / sizeof(A[0]);
+    display(A, n);
+    mergeSort(A, 0, n - 1);
     display(A, n);
     return 0;
 }
