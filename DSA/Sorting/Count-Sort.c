d23ed6e52aa4cd9033c327da4caab5dcb7e0c60b
Parth Vighne <parthvighne@gmail.com>
2022-06-21 19:32:41 +0530

    count sort

---
+++ DSA/Sorting/Count-Sort.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <limits.h>
+
+int maximum(int A[], int n)
+{
+    int max = INT_MIN;
+    for (int i = 0; i < n; i++)
+    {
+        if (A[i] > max)
+            max = A[i];
+    }
+    return max;
+}
+
+void countSort(int *A, int n)
+{
+    int i, j;
+    // finding the biggest element in array
+    int max = maximum(A, n); // size of count array
+
+    // creating "count" array of size max
+    int *count = (int *)malloc((max + 1) * sizeof(int));
+
+    // putting every element's value to 0
+    for (i = 0; i <= max; i++)
+    {
+        count[i] = 0;
+    }
+
+    // Incrementing the corresponding index in the count array
+    for (i = 0; i < n; i++)
+    {
+        count[A[i]]++; // the value in A is the index of count
+    }
+
+    i = 0;
+    j = 0;
+
+    while (i <= max)
+    {
+        if (count[i] > 0) // if element in count has some iteration
+        {
+            A[j] = i;   // copy index of count i.e the value of element of count to array A
+            j++;        // increment to next element in A
+            count[i]--; // decrement the count of the element
+        }
+        else
+            i++; // increment to next element in count
+    }
+}
+
+void display(int A[], int n)
+{
+    printf("\n- - - - - - \n");
+    for (int i = 0; i < n; i++)
+        printf("%d ", A[i]);
+    printf("\n");
+}
+
+int main()
+{
+    int A[] = {9, 1, 4, 14, 4, 15, 6};
+    int n = 7;
+    display(A, n);
+    countSort(A, n);
+    display(A, n);
+    return 0;
+}
